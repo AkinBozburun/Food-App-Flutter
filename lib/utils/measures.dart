b9dd08476f859a9df70fa666e3175abf11e53ac9
Akın Bozburun <a_bozburun@hotmail.com>
2023-11-07 23:47:45 +0300

    new measures has been added.

--- lib/utils/measures.dart
+++ lib/utils/measures.dart
@@ -6,8 +6,9 @@ class Measures
   static BorderRadius border12 = BorderRadius.circular(12);
   static BorderRadius border16 = BorderRadius.circular(16);
   static BorderRadius border24 = BorderRadius.circular(24);
-  static EdgeInsets all16 = const EdgeInsets.all(16);
 
+  static EdgeInsets all8 = const EdgeInsets.all(8);
+  static EdgeInsets all16 = const EdgeInsets.all(16);
   static EdgeInsets horizontal0 =  const EdgeInsets.symmetric(horizontal: 0);
   static EdgeInsets horizontal8 = const EdgeInsets.symmetric(horizontal: 8);
   static EdgeInsets horizontal16 = const EdgeInsets.symmetric(horizontal: 16);
